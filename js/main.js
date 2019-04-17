71615881f45abe564d8cb6a9d826accd5ed4cf93
Alex Megalokonomos <alex@clockwork.gr>
2019-04-18 01:22:53 +0300

    API connection

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-$( document ).ready(function() {
-    var paylink = window.location.hash.replace('#','');
+$(document).ready(function () {
+    var paylink = window.location.hash.replace('#', '');
     var params = paylink.split('/');
-    var amount=params[0];
-    var asset=params[1];
-    var account=params[2];
-    console.log(params);
+    var amount = params[0];
+    var asset = params[1];
+    var account = params[2];
+    bitshares_js.bitshares_ws.Apis.instance("wss://bts-seoul.clockwork.gr", true).init_promise.then(function (res) {
+        bitshares_js.bitshares_ws.Apis.instance().db_api().exec("get_accounts", [[params[2]]]).then(function (account) {
+            console.log(account);
+        });
+    });
 });
