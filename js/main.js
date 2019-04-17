109451e4ef54ca9a181d5a4e2b9fc6bf2a22e314
Alex Megalokonomos <alex@clockwork.gr>
2019-04-18 01:33:53 +0300

    asset to uppercase

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
     var paylink = window.location.hash.replace('#', '');
     var params = paylink.split('/');
     var amount = params[0];
-    var asset = params[1];
+    var asset = params[1].toUpperCase();
     var account = params[2];
     bitshares_js.bitshares_ws.Apis.instance("wss://bts-seoul.clockwork.gr", true).init_promise.then(function (res) {
-        bitshares_js.bitshares_ws.Apis.instance().db_api().exec("get_accounts", [[params[2]]]).then(function (account) {
-            console.log(account);
-            bitshares_js.bitshares_ws.Apis.instance().db_api().exec("get_assets", [[params[1]]]).then(function (asset) {
-                console.log(asset);
+        bitshares_js.bitshares_ws.Apis.instance().db_api().exec("get_accounts", [[account]]).then(function (details) {
+            console.log(details);
+            bitshares_js.bitshares_ws.Apis.instance().db_api().exec("get_assets", [[asset]]).then(function (asset_details) {
+                console.log(asset_details);
             });
         });
     });
